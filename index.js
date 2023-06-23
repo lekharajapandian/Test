a808d97896c13ccc1262f0739a8978a8dd12adb8
lekharajapandian <137245083+lekharajapandian@users.noreply.github.com>
2023-06-23 15:36:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+function Fun(props){
+    return(
+        <h1>Hello{props.brand.number}{props.brand.dob}</h1>
+    )
+}
+const shoot = (a) => console.log(a);
+
+function Fun1(){
+    const namee = "VennilaGuhan"
+    const obj = { dob : "Feb 23", number : 79798 }
+    return(
+       <>
+        <h1>Hello</h1>
+         <Fun brand = { obj } />
+         <button onClick={()=>shoot("Goal")}>Click</button>
+         
+    </>
+    )
+
+   
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Fun1/>);
